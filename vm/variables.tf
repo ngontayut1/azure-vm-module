9aa52ee8767ffd3774bd471093318f8e72d27aaf
ngontayut1 <61141144+ngontayut1@users.noreply.github.com>
2024-12-27 18:19:20 +0700

    Update variables.tf

--- vm/variables.tf
+++ vm/variables.tf
@@ -1,1 +1,12 @@
-
+variable "vm_list" {
+  description = "List of VM configurations"
+  type = list(object({
+    vm_name              = string
+    location             = string
+    resource_group_name  = string
+    network_interface_id = string
+    vm_size              = string
+    admin_username       = string
+    admin_password       = string
+  }))
+}
